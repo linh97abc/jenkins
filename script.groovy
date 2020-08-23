e82fb5161ebba9757b927261fc959ed655198fb4
linh97abc <linh97abc@gmail.com>
2020-08-23 20:31:00 +0700

    fix script3

--- script.groovy
+++ script.groovy
@@ -1,24 +1,24 @@
 def west_init() {
     sh 'mkdir -p modules/hal/cmsis'
-    dir('modules/hal/cmsis'){                    
+    dir('modules/hal/cmsis')(                   
         git url: 'https://github.com/linh97abc/HN20_FR_EMB_02.git',
         branch: 'master',
         // credentialsId: 'aaa'
-    }
+    )
 
     sh 'mkdir -p modules/hal/nxp'
-    dir('modules/hal/nxp'){
+    dir('modules/hal/nxp')(
         git url: 'https://github.com/linh97abc/test.git',
         branch: 'master',
         // credentialsId: 'aaa'
-    } 
+    )
 
     sh 'mkdir -p zephyr'
-    dir('zephyr'){
+    dir('zephyr')(
         git url: 'https://github.com/linh97abc/test.git',
         branch: 'master',
         // credentialsId: 'aaa'
-    }
+    )
 
     sh 'mkdir -p .west'
     sh 'echo \"[manifest]\\r\\npath=zephyr\\r\\n[zephyr]\\r\\nbase=zephyr\">.west/config'
